33cfa7f427a711d1ea321d860584ac4632cab848
robtplayground <rob.thwaites@playgroundxyz.com>
2017-05-24 09:45:43 +1000

    Bugfix

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-var data = require('./content/content.json');
-// data += require('./content/content2.json');
-// console.log(data);
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
